2f8bd85a1ecad19a7672ad1ea75889323c0a5c33
miguelfmproject <miguel.dfm4@gmail.com>
2026-07-12 20:03:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+from binance_ves import obter_taxas_ves
+from bybit_brl import obter_taxas_brl
 
+
+def main():
+
+    print("===================================")
+    print("      TAXAS P2P")
+    print("===================================")
+
+    ves = obter_taxas_ves()
+    brl = obter_taxas_brl()
+
+    print("\nResultado VES:")
+    print(ves)
+
+    print("\nResultado BRL:")
+    print(brl)
+
+
+if __name__ == "__main__":
+    main()
