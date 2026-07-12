f80b94ef14ce55d1045b579975e95934b8da23c7
miguelfmproject <miguel.dfm4@gmail.com>
2026-07-12 20:05:34 -0300

    Update binance_ves.py

--- binance_ves.py
+++ binance_ves.py
@@ -1,1 +1,11 @@
+def obter_taxas_ves():
+    """
+    Busca as taxas P2P da Binance (VES).
+    """
 
+    return {
+        "buy": None,
+        "sell": None,
+        "buy_method": None,
+        "sell_method": None
+    }
