ce05172192064e7d65a1827602b743bbe68e6e22
miguelfmproject <miguel.dfm4@gmail.com>
2026-07-13 01:38:18 -0300

    Update sheets.py

--- sheets.py
+++ sheets.py
@@ -1,9 +1,8 @@
 import gspread
-
 from google.oauth2.service_account import Credentials
+import traceback
 
 from config import SPREADSHEET_ID
-
 
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -11,31 +10,43 @@ SCOPES = [
 ]
 
 
-credenciais = Credentials.from_service_account_file(
-    "credentials/service_account.json",
-    scopes=SCOPES
-)
+def conectar():
 
-cliente = gspread.authorize(credenciais)
+    try:
 
-planilha = cliente.open_by_key(SPREADSHEET_ID)
+        credenciais = Credentials.from_service_account_file(
+            "credentials/service_account.json",
+            scopes=SCOPES
+        )
 
-aba = planilha.sheet1
+        cliente = gspread.authorize(credenciais)
+
+        planilha = cliente.open_by_key(SPREADSHEET_ID)
+
+        return planilha.sheet1
+
+    except Exception:
+
+        print("\nERRO AO CONECTAR COM GOOGLE SHEETS\n")
+
+        traceback.print_exc()
+
+        raise
 
 
 def atualizar_ves(compra, venda, metodo_compra, metodo_venda):
 
-    aba.update("I8", compra)
+    aba = conectar()
 
-    aba.update("J8", venda)
-
-    aba.update("I10", metodo_compra)
-
-    aba.update("J10", metodo_venda)
+    aba.update("I8", [[compra]])
+    aba.update("J8", [[venda]])
+    aba.update("I10", [[metodo_compra]])
+    aba.update("J10", [[metodo_venda]])
 
 
 def atualizar_brl(compra, venda):
 
-    aba.update("K8", compra)
+    aba = conectar()
 
-    aba.update("L8", venda)
+    aba.update("K8", [[compra]])
+    aba.update("L8", [[venda]])
