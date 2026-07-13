6a415f6ae5fd219db67a1759b8a39c5a9d83c828
miguelfmproject <miguel.dfm4@gmail.com>
2026-07-12 22:48:37 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,24 @@
-# Google Sheets
+# =====================================================
+# CONFIGURAÇÕES DO PROJETO
+# =====================================================
 
+# ID da planilha Google Sheets
 SPREADSHEET_ID = "1GJz57eO5uBvkz7sfrrcK72EvVRzXwZTDCemHPLN053U"
 
-# Células
+# Nome da aba da planilha
+SHEET_NAME = "Página1"
 
+# =====================================================
+# CÉLULAS
+# =====================================================
+
+# Binance (VES)
 CELL_VES_BUY = "I8"
 CELL_VES_SELL = "J8"
 
 CELL_VES_BUY_METHOD = "I10"
 CELL_VES_SELL_METHOD = "J10"
 
+# Bybit (BRL)
 CELL_BRL_BUY = "K8"
 CELL_BRL_SELL = "L8"
