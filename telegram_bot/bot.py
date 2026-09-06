f09c668895134cae632f0f069357e8d5fc836fe0
miguelfmproject <miguel.dfm4@gmail.com>
2026-09-06 09:10:23 -0300

    Update bot.py

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -1,1 +1,21 @@
+import os
+import requests
 
+
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+if not TOKEN:
+    raise ValueError("A variável TELEGRAM_BOT_TOKEN não foi configurada.")
+
+
+def main():
+    url = f"https://api.telegram.org/bot{TOKEN}/getMe"
+
+    response = requests.get(url)
+    data = response.json()
+
+    print(data)
+
+
+if __name__ == "__main__":
+    main()
